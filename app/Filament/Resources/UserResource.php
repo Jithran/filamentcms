e307ffc48ce2c0431df2c25a234802f80e681015
Jithran <info@jithransikken.nl>
2023-04-19 22:51:23 +0200

    add user resource controll

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Support\Facades\Hash;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationGroup = 'Users';
+
+    protected static ?string $navigationLabel = 'User Management';
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Card::make()->schema([
+                    Forms\Components\TextInput::make('name')
+                        ->autofocus()
+                        ->required(),
+                    Forms\Components\TextInput::make('email')
+                        ->required(),
+                    Forms\Components\TextInput::make('password')
+                        ->type('password')
+                        ->required(fn($record): bool => is_null($record?->id))
+                        ->minLength(8)
+                        ->same('password_confirmation')
+                        ->dehydrateStateUsing(fn($state): string => Hash::make($state))
+                        ->dehydrated(fn($state) => filled($state)),
+                    Forms\Components\TextInput::make('password_confirmation')
+                        ->type('password')
+                        ->required(fn($record): bool => is_null($record?->id))
+                        ->minLength(8)
+                        ->dehydrated(false),
+                ]),
+            ]);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                'Name' => TextColumn::make('name')
+                    ->sortable()
+                    ->searchable(),
+                'Email' => TextColumn::make('email')
+                    ->sortable()
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Pages;
+
+use App\Filament\Resources\UserResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditUser extends EditRecord
+{
+    protected static string $resource = UserResource::class;
+
+    // process the request, save the record, and redirect to the redirect URL
+    // handle the request, only change password if it's not empty
+
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
